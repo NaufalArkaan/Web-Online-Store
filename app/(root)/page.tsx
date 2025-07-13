869e4f91446aad1349ae14a895596e43b28bc5de
NaufalArkaan <naufalarkaan25@gmail.com>
2025-07-13 21:22:53 +0700

    Authentication Clerk

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="p-4">
+      <Button variant= "destructive" size="lg">
+          Click me
+      </Button>
+    </div>
+  );
+}
+
