054f917ad447322d59da99bc2457e4d989d52989
NaufalArkaan <naufalarkaan25@gmail.com>
2025-07-14 22:58:27 +0700

    add modal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,26 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+'use client'
 
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import Modal from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import { useEffect } from "react";
+
+const SetupPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen)
+  const isOpen = useStoreModal((state) => state.isOpen)
+
+  useEffect(() => {
+    if(!isOpen){
+      onOpen()
+    }
+  },[isOpen, onOpen])
   return (
     <div className="p-4">
-      <Button variant= "destructive" size="lg">
-          Click me
-      </Button>
+      Root Page
     </div>
   );
 }
 
+export default SetupPage;
